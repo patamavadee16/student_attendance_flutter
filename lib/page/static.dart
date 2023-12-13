9e91a790b3f3b52c04ec69c3eda66ab7cd507abb
Patamavadee Tubthong <104760464+patamavadee16@users.noreply.github.com>
2023-12-13 23:42:36 +0700

    fetch student

--- lib/page/static.dart
+++ lib/page/static.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'dart:ui' as ui;
+import 'package:syncfusion_flutter_xlsio/xlsio.dart' as xcel;
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
+import 'dart:io';
 
 class staticPage extends StatefulWidget {
   const staticPage({super.key});
@@ -12,6 +16,7 @@ class staticPage extends StatefulWidget {
 class _staticPageState extends State<staticPage> {
   String dropdownValue = '0';
   final List _course = [];
+    final List student = [];
   bool isSelect = false;
   List<DropdownMenuItem> courseItems = [];
   final _firestoreInstance = FirebaseFirestore.instance;
@@ -56,12 +61,9 @@ class _staticPageState extends State<staticPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: 
-   
-
-      Column(
+      body: Column(
         children: [
-            Container(
+          Container(
             padding: EdgeInsets.only(top: 25),
             child: SizedBox(
               width: MediaQuery.of(context).size.width,
@@ -121,81 +123,211 @@ class _staticPageState extends State<staticPage> {
                 ),
               ),
             ),
-          ),DropdownButtonFormField(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 5, vertical: 5),
-                          borderRadius: BorderRadius.circular(12.0),
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(13),
-                              borderSide: const BorderSide(
-                                  color: ui.Color.fromARGB(255, 255, 255, 255),
-                                  width: 2),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(13),
-                              borderSide: const BorderSide(
-                                  color: ui.Color.fromARGB(255, 255, 255, 255),
-                                  width: 3),
-                            ),
-                          ),
-                          items: courseItems,
-                          value: dropdownValue,
-                          onChanged: (codeValue) {
-                            setState(() {
-                              dropdownValue=codeValue;
-                              isSelect=true;
-                              // dropdownValue = codeValue;
+          ),
+          DropdownButtonFormField(
+            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+            borderRadius: BorderRadius.circular(12.0),
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(13),
+                borderSide: const BorderSide(
+                    color: ui.Color.fromARGB(255, 255, 255, 255), width: 2),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(13),
+                borderSide: const BorderSide(
+                    color: ui.Color.fromARGB(255, 255, 255, 255), width: 3),
+              ),
+            ),
+            items: courseItems,
+            value: dropdownValue,
+            onChanged: (codeValue) {
+              setState(() {
+                dropdownValue = codeValue;
+                isSelect = true;
+                // dropdownValue = codeValue;
 
-                              // print(dropdownValue);
-                              // subject = codeValue['titleTH'];
-                            });
-                          },
-                          isExpanded: false,
-                        ),
-          isSelect? SizedBox(
-            height: 600,
-            child: newMethod(context)):Text('data')
+                // print(dropdownValue);
+                // subject = codeValue['titleTH'];
+              });
+            },
+            isExpanded: false,
+          ),
+          isSelect
+              ? Column(
+                  children: [
+                    SizedBox(height: 450, child:   FutureBuilder<Widget>(
+       future: fetchStudennt(),
+       builder: (BuildContext context, AsyncSnapshot<Widget> snapshot){
+         if(snapshot.hasData) {
+           return            ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: student.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Text(
+                      student[index]['stdId']+student[index]['name']+''+index.toString(),
+                    );
+                  });
+         }
+
+         return Center(child: CircularProgressIndicator());
+       }
+      ),),
+                    ElevatedButton(
+                        onPressed: createExcel, child: Icon(Icons.download))
+                  ],
+                )
+              : Text('select ')
         ],
       ),
- 
-  );
+    );
+  }
 
-    
+  void createExcel() async {
+    var myList = [
+      {
+        "id": "116310400477-0",
+        "name": "ปฐมาวดี ทับทอง",
+        "count": "10"
+      },
+      {
+        "id": "116310400477-0",
+        "name": "ปฐมาวดี ทับทอง",
+        "count": "10"
+      },
+      {
+        "id": "116310400477-0",
+        "name": "ปฐมาวดี ทับทอง",
+        "count": "10"
+      },
+      {
+        "id": "116310400477-0",
+        "name": "ปฐมาวดี ทับทอง",
+        "count": "10"
+      },
+      {
+        "id": "116310400477-0",
+        "name": "ปฐมาวดี ทับทอง",
+        "count": "10"
+      },
+      {
+        "id": "116310400477-0",
+        "name": "ปฐมาวดี ทับทอง",
+        "count": "10"
+      },
+      {
+        "id": "116310400477-0",
+        "name": "ปฐมาวดี ทับทอง",
+        "count": "10"
+      },
+      {
+        "id": "116310400477-0",
+        "name": "ปฐมาวดี ทับทอง",
+        "count": "10"
+      }
+    ];
+    final xcel.Workbook workbook = xcel.Workbook();
+    final xcel.Worksheet sheet = workbook.worksheets[0];
+    sheet.getRangeByIndex(1, 1).setText("รหัสนักศึกษา");
+    sheet.getRangeByIndex(1, 2).setText("ชื่อ-นามสกุล");
+    sheet.getRangeByIndex(1, 3).setText("สถิติ");
+    for (var i = 0; i < student.length; i++) {
+      final item = student[i];
+      sheet.getRangeByIndex(i + 2, 1).setText(item["stdId"].toString());
+      sheet.getRangeByIndex(i + 2, 2).setText(item["name"].toString());
+      sheet.getRangeByIndex(i + 2, 3).setText(item["id"].toString());
+    }
+    final List<int> bytes = workbook.saveAsStream();
+    // xcel.Workbook().dispose();
+
+    final String path = (await getApplicationSupportDirectory()).path;
+    final String fileName = '/storage/emulated/0/Download/static1.xlsx';
+    final File file = File(fileName);
+    print(path);
+    await file.writeAsBytes(bytes, flush: true);
+    // xcel.OpenFile.open(fileName);
   }
 
   StreamBuilder<QuerySnapshot> newMethod(BuildContext context) {
     List<String> Items = [];
     return StreamBuilder<QuerySnapshot>(
-      
         stream: _firestoreInstance
             .collection('course')
             .doc(dropdownValue)
-            .collection('students').orderBy('studentId')
+            .collection('students')
+            .orderBy('no')
             .snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
             const CircularProgressIndicator();
           } else {
             final codeItem = snapshot.data?.docs.reversed.toList();
+            Items.clear();
             for (var code in codeItem!) {
-              Items.add(code['name']);
+              Items.add(code['studentId'] + ' ' + code['name']);
               print(code['name']);
             }
           }
           return
               // Text('data');
- ListView.builder(
-                    scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                          itemCount: Items.length,
-                          itemBuilder: (BuildContext context, int index) {
-                return Text(
-                 Items[index]+index.toString(),
-                );
-                          
-              
-        });});
-  
+              ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: Items.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Text(
+                      Items[index] + index.toString(),
+                    );
+                  });
+        });
+  }
+    Future<Widget> fetchStudennt() async {
+    QuerySnapshot qn = await _firestoreInstance
+          .collection('course')
+            .doc(dropdownValue)
+            .collection('students')
+            .orderBy('no')
+        .get();
+    setState(() {
+      student.clear();
+      for (int i = 0; i < qn.docs.length; i++) {
+        student.add({
+          'id': qn.docs[i].id,
+          "name": qn.docs[i]["name"],
+          "stdId": qn.docs[i]['studentId'],
+          'no': qn.docs[i]['no'],
+          'attendance':qn.docs[i]['attendance']
 
-}}
+
+        });
+  // int sum = student[i]['attendance'].fold(0, (acc, map) {
+  //   bool boolValue = map.values.first; // Assuming each map has only one key-value pair
+  //   return acc + (boolValue ? 1 : 0);
+  // });
+
+  // calculate the sum with a loop
+  // for (int value in values) {
+  //   sum += value;
+  // }
+// print(sum );
+print(student[i]['attendance']);
+      }
+
+ 
+      
+    });
+         return  ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: student.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Text(
+                      student[index]['stdId']+index.toString(),
+                    );
+                  });
+   
+              // Text('data');
+   
+  }
+}
